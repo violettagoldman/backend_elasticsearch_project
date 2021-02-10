9f226b630491c7f509492c3d03e850da2f273fa7
Romane <romaneleseur2@gmail.com>
2021-02-10 15:29:50 +0100

    Get local and online CSV files

--- backend_elasticsearch_project/src/main/java/jar/App.java
+++ backend_elasticsearch_project/src/main/java/jar/App.java
@@ -1,13 +1,26 @@
 package jar;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import java.util.List;
+
+public class App {
+
+    public static void main( String[] args ) throws IOException {
+
+        File file = CSVParser.getResource("test.csv");
+
+        URL url = new URL("https://data.cityofnewyork.us/api/views/7yq2-hq9c/rows.csv");
+        //http://winterolympicsmedals.com/medals.csv
+
+        List contentLocal = CSVParser.readFileLocal(file);
+        List contentURL = CSVParser.readFileURL(url);
+
+        System.out.println(contentLocal);
+        System.out.println(contentURL);
+
     }
+
 }
--- backend_elasticsearch_project/src/main/java/jar/CSVParser.java
+++ backend_elasticsearch_project/src/main/java/jar/CSVParser.java
@@ -1,4 +1,53 @@
 package jar;
 
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CSVParser {
+
+    public static File getResource(String fileName) {
+        final File initFile = new File("");
+        final String pathFileName = initFile.getAbsolutePath() + File.separator + fileName;
+
+        File file = new File(pathFileName);
+        return file;
+    }
+
+    public static List<String> readFileLocal(File file) throws IOException {
+
+        List<String> result = new ArrayList<>();
+
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
+            result.add(line);
+        }
+
+        bufferedReader.close();
+        fileReader.close();
+
+        return result;
+    }
+
+    public static List<String> readFileURL(URL url) throws IOException {
+
+        List<String> result = new ArrayList<>();
+
+        URLConnection connection = url.openConnection();
+        InputStreamReader input = new InputStreamReader(connection.getInputStream());
+        BufferedReader bufferedReader = new BufferedReader(input);
+
+        for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
+            result.add(line);
+        }
+
+        bufferedReader.close();
+
+        return result;
+    }
+
 }
