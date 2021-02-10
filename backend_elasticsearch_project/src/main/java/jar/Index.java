1b7dabdbed7d022954e5750458aad597d728f48f
unknown <yoyoalternatif@gmail.com>
2021-02-10 15:53:29 +0100

    index

--- backend_elasticsearch_project/src/main/java/jar/Index.java
+++ backend_elasticsearch_project/src/main/java/jar/Index.java
@@ -1,13 +1,58 @@
 package jar;
 
+import java.util.HashMap;
+
 public class Index
 {
-    /* valeur
-    nbfois où il apprait
-    liste des ligne
+    public static void main(String[] argv)
+    {
+        Index test = new Index("testnomIndex", "testnomTable", new HashMap<Integer, String>());
+        addColumn(1,"testNomColumn1", test.getNameColumns());
+        addColumn(2,"testNomColumn2", test.getNameColumns());
+        addColumn(3,"testNomColumn3", test.getNameColumns());
+        System.out.println(test.getIndex());
+        deleteColumn(3, test.getNameColumns());
+        System.out.println(test.getIndex());
+    }
 
-    where utilise l'index pour aller plus vite
-    a la fin pas de hashmap
-    */
-    
+    private String nameIndex;
+    private String nameTable;
+    private HashMap<Integer, String> nameColumns;
+
+    public Index(String nomIndex, String nomTable, HashMap<Integer, String> nomColumn) {
+        this.nameIndex = nomIndex;
+        this.nameTable = nomTable;
+        this.nameColumns = new HashMap<Integer, String>();
+    }
+
+    public static HashMap<Integer, String> addColumn(Integer columnNumber, String nomColumnToAdd, HashMap<Integer, String> hashMap){
+        hashMap.put(columnNumber, nomColumnToAdd);
+        return hashMap;
+    }
+
+    public static HashMap<Integer, String> deleteColumn(Integer columnNumber, HashMap<Integer, String> hashMap){
+        hashMap.remove(columnNumber);
+        return hashMap;
+    }
+    public HashMap<Integer, String> getNameColumns() {
+        return nameColumns;
+    }
+
+    public String getNameIndex() {
+        return nameIndex;
+    }
+
+    public String getNameTable() {
+        return nameTable;
+    }
+
+    public String getIndex() {
+        return "Index{" +
+                "nomIndex=" + getNameIndex() +
+                ", nomTable='" + getNameTable() +
+                ", nomColumn='" + getNameColumns() +
+                '}';
+    }
 }
+
+
