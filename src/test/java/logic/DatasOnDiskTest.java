d07fbc367f7339a9c300993b12a14134045eb7b0
Romane <romaneleseur2@gmail.com>
2021-05-25 19:07:39 +0200

    Amélioration du test unitaire de DatasOnDisk

--- src/test/java/logic/DatasOnDiskTest.java
+++ src/test/java/logic/DatasOnDiskTest.java
@@ -15,15 +15,15 @@ import static org.junit.Assert.assertEquals;
 
 public class DatasOnDiskTest {
 
-    DatasOnDisk dod = new DatasOnDisk();
-    CSVParser csvp = new CSVParser();
-    File file = new File("src/resources/dogs.csv");
+    private DatasOnDisk dod = new DatasOnDisk();
+    private CSVParser csvp = new CSVParser();
+    private File file = new File("src/resources/dogs.csv");
 
     public DatasOnDiskTest() throws FileNotFoundException {
     }
 
     @Test
-    public void writeAndRead() throws IOException {
+    public void write_and_read_all_csv() throws IOException {
         List<String> csvContent = csvp.readFileLocal(file);
         List<String[]> lines = new ArrayList<>();
 
@@ -52,11 +52,53 @@ public class DatasOnDiskTest {
         for (int i = 0; i < 10; i++) {
             data = dod.readLine(i, columns);
             line = lines.get(i);
-            for (int j = 0; j < 4; j++) {
+            for (int j = 0; j < columns.size(); j++) {
                 assertEquals(data[j], line[j]);
             }
         }
 
+    }
+
+    @Test
+    public void read_part_csv() throws IOException {
+        List<String> csvContent = csvp.readFileLocal(file);
+        List<String[]> lines = new ArrayList<>();
+
+        for (String line : csvContent) {
+            String[] lineSplited = line.split(",");
+            lines.add(lineSplited);
+        }
+
+        Column id = new Column("Id", "int");
+        Column prenom = new Column("Prenom", "String");
+        Column couleur = new Column("couleur", "String");
+        Column age = new Column("age", "int");
+
+        ArrayList<Column> columns = new ArrayList<>();
+        columns.add(id);
+        columns.add(prenom);
+        columns.add(couleur);
+        columns.add(age);
+
+        for (String[] line : lines) {
+            dod.writeLine(line, columns);
+        }
+
+        Object[] data = dod.readLine(1, columns);
+        String[] line = lines.get(1);
+        for (int i = 0; i < columns.size(); i++){
+            System.out.println(data[i]);
+            System.out.println(line[i]);
+            assertEquals(data[i], line[i]);
+        }
+
+        data = dod.readLine(5, columns);
+        line = lines.get(5);
+        for (int i = 0; i < columns.size(); i++){
+            System.out.println(data[i]);
+            System.out.println(line[i]);
+            assertEquals(data[i], line[i]);
+        }
     }
 
 }
