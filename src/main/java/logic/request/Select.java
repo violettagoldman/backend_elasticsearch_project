2fb5c026a4f0f9b50dd5c043154f0149f504d1e9
JeanneBarrirere <barriere.jeanne@gmail.com>
2021-05-27 22:14:12 +0200

    modification de nom dans fonction pour plus de claireté

--- src/main/java/logic/request/Select.java
+++ src/main/java/logic/request/Select.java
@@ -14,7 +14,6 @@ import java.util.*;
  * class that implements the request select
  */
 public class Select {
-    private List rows;
     private Table result;
 
     /**
@@ -24,37 +23,36 @@ public class Select {
      * @param from
      */
     public Select(ArrayList rows, String [] columnsName, From from) throws IOException {
-        this.rows = rows;
         Table table = from.table;
         result = table.clone(columnsName);
-        //Map<String, Column> columns = new TreeMap<String, Column>();
         DatasOnDisk dod = new DatasOnDisk();
         for (Object row: rows) {
-            HashMap<String, String > list = new HashMap<>();
-            List<String> aList = Arrays.asList(columnsName);
+            HashMap<String, String > dataList = new HashMap<>();
+            List<String> columnsNamesList = Arrays.asList(columnsName);
             for (String name: columnsName) {
                 // take data if they are index
                 if(result.getIndex().get(name)!=null){
-                    list.put(name, result.getIndex().get(name).getData((int)row) );
-                    aList.remove(name);
+                    dataList.put(name, result.getIndex().get(name).getData((int)row) );
+                    columnsNamesList.remove(name);
                 }
             }
             // take the rest of data in disk
             ArrayList<Column> listColumn = new ArrayList<>();
-            for (String name : aList) {
+            for (String name : columnsNamesList) {
                 listColumn.add(result.getColumns().get(name));
             }
             String [] data = (String[]) dod.readLine((int)row, result.getColumnsList(), listColumn);
             for(int i = 0; i<data.length; i++) {
-                list.put(aList.get(i), data[i]);
+                dataList.put(columnsNamesList.get(i), data[i]);
             }
-            result.addLineColumn(list);
+            // add the line in the table
+            result.addLineColumn(dataList);
         }
     }
 
     /**
      * return the result
-     * @return
+     * @return table contains result
      */
     public Table getResult() {
         return result;
