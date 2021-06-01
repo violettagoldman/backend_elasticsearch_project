681a4eeae8296cffe4e1a6d4ae46d51f322161ce
Romane <romaneleseur2@gmail.com>
2021-06-01 21:24:37 +0200

    Test unitaires DatasOnDisk et CSVParcer

--- src/test/java/logic/DatasOnDiskTest.java
+++ src/test/java/logic/DatasOnDiskTest.java
@@ -1,106 +1,180 @@
-/*
 package logic;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import parser.CSVParser;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class DatasOnDiskTest {
 
     private DatasOnDisk dod = new DatasOnDisk();
-    private CSVParser csvp = new CSVParser();
     private File file = new File("src/resources/dogs.csv");
 
     public DatasOnDiskTest() throws FileNotFoundException {
     }
 
     @Test
-    public void write_and_read_all_csv() throws IOException {
-        List<String> csvContent = csvp.readFileLocal(file);
-        List<String[]> lines = new ArrayList<>();
+    public void write_and_read_all_file() throws IOException {
+        ArrayList<String> colsNames = new ArrayList<>();
+        colsNames.add("id");
+        colsNames.add("name");
+        colsNames.add("color");
+        colsNames.add("age");
 
-        for (String line : csvContent) {
-            String[] lineSplited = line.split(",");
-            lines.add(lineSplited);
-        }
+        ArrayList<String> colsType = new ArrayList<>();
+        colsType.add("int");
+        colsType.add("String");
+        colsType.add("String");
+        colsType.add("int");
+
+        Table table = new Table("dogs", colsNames, colsType);
 
         Column id = new Column("Id", "int");
         Column prenom = new Column("Prenom", "String");
         Column couleur = new Column("couleur", "String");
         Column age = new Column("age", "int");
 
-        ArrayList<Column> columns = new ArrayList<>();
-        columns.add(id);
-        columns.add(prenom);
-        columns.add(couleur);
-        columns.add(age);
+        ArrayList<Column> cols = new ArrayList<>();
+        cols.add(id);
+        cols.add(prenom);
+        cols.add(couleur);
+        cols.add(age);
 
-        for (String[] line : lines) {
-            dod.writeLine(line, columns);
-        }
+        String[] line1 = {"1", "Titi", "Jaune", "5"};
+        String[] line2 = {"2", "Medor", "Noir", "10"};
+        String[] line3 = {"3", "Pitie", "Noir", "5"};
+        String[] line4 = {"4", "Juju", "Gris", "5"};
+        String[] line5 = {"5", "Vanille", "Blanc", "7"};
+        String[] line6 = {"6", "Chocolat", "Marron", "12"};
+        String[] line7 = {"7", "Milou", "Blanc", "3"};
+        String[] line8 = {"8", "Idefix", "Blanc", "14"};
+        String[] line9 = {"9", "Pluto", "Jaune", "17"};
+        String[] line10 = {"10", "Dingo", "Roux", "1"};
+
+        dod.writeLine(line1, table);
+        dod.writeLine(line2, table);
+        dod.writeLine(line3, table);
+        dod.writeLine(line4, table);
+        dod.writeLine(line5, table);
+        dod.writeLine(line6, table);
+        dod.writeLine(line7, table);
+        dod.writeLine(line8, table);
+        dod.writeLine(line9, table);
+        dod.writeLine(line10, table);
+
+        ArrayList<String[]> datas = new ArrayList<>();
+        datas.add(line1);
+        datas.add(line2);
+        datas.add(line3);
+        datas.add(line4);
+        datas.add(line5);
+        datas.add(line6);
+        datas.add(line7);
+        datas.add(line8);
+        datas.add(line9);
+        datas.add(line10);
 
         Object[] data;
-        String[] line;
         for (int i = 0; i < 10; i++) {
-            data = dod.readLine(i, columns);
-            line = lines.get(i);
-            for (int j = 0; j < columns.size(); j++) {
-                assertEquals(data[j], line[j]);
+            data = dod.readLine(i, cols, cols);
+            for (int j = 0; j < 4; j++) {
+                assertEquals(datas.get(i)[j], data[j]);
+                System.out.print(data[j] + ", ");
             }
+            System.out.print("\n");
         }
-
     }
 
     @Test
-    public void read_part_csv() throws IOException {
-        List<String> csvContent = csvp.readFileLocal(file);
-        List<String[]> lines = new ArrayList<>();
+    public void write_and_read_part_file() throws IOException {
+        ArrayList<String> colsNames = new ArrayList<>();
+        colsNames.add("id");
+        colsNames.add("name");
+        colsNames.add("color");
+        colsNames.add("age");
 
-        for (String line : csvContent) {
-            String[] lineSplited = line.split(",");
-            lines.add(lineSplited);
-        }
+        ArrayList<String> colsType = new ArrayList<>();
+        colsType.add("int");
+        colsType.add("String");
+        colsType.add("String");
+        colsType.add("int");
+
+        Table table = new Table("dogs", colsNames, colsType);
 
         Column id = new Column("Id", "int");
         Column prenom = new Column("Prenom", "String");
         Column couleur = new Column("couleur", "String");
         Column age = new Column("age", "int");
 
-        ArrayList<Column> columns = new ArrayList<>();
-        columns.add(id);
-        columns.add(prenom);
-        columns.add(couleur);
-        columns.add(age);
+        ArrayList<Column> cols = new ArrayList<>();
+        cols.add(id);
+        cols.add(prenom);
+        cols.add(couleur);
+        cols.add(age);
 
-        for (String[] line : lines) {
-            dod.writeLine(line, columns);
+        ArrayList<Column> colsSelected = new ArrayList<>();
+        colsSelected.add(id);
+        colsSelected.add(prenom);
+
+        String[] line1 = {"1", "Titi", "Jaune", "5"};
+        String[] line2 = {"2", "Medor", "Noir", "10"};
+        String[] line3 = {"3", "Pitie", "Noir", "5"};
+        String[] line4 = {"4", "Juju", "Gris", "5"};
+        String[] line5 = {"5", "Vanille", "Blanc", "7"};
+        String[] line6 = {"6", "Chocolat", "Marron", "12"};
+        String[] line7 = {"7", "Milou", "Blanc", "3"};
+        String[] line8 = {"8", "Idefix", "Blanc", "14"};
+        String[] line9 = {"9", "Pluto", "Jaune", "17"};
+        String[] line10 = {"10", "Dingo", "Roux", "1"};
+
+        dod.writeLine(line1, table);
+        dod.writeLine(line2, table);
+        dod.writeLine(line3, table);
+        dod.writeLine(line4, table);
+        dod.writeLine(line5, table);
+        dod.writeLine(line6, table);
+        dod.writeLine(line7, table);
+        dod.writeLine(line8, table);
+        dod.writeLine(line9, table);
+        dod.writeLine(line10, table);
+
+        ArrayList<String[]> datas = new ArrayList<>();
+        datas.add(line1);
+        datas.add(line2);
+        datas.add(line3);
+        datas.add(line4);
+        datas.add(line5);
+        datas.add(line6);
+        datas.add(line7);
+        datas.add(line8);
+        datas.add(line9);
+        datas.add(line10);
+
+        Object[] data;
+
+        data = dod.readLine(0, cols, colsSelected);
+        for (int i = 0; i < colsSelected.size(); i++) {
+            assertEquals(datas.get(0)[i], data[i]);
+            System.out.print(data[i] + ", ");
         }
+        System.out.print("\n");
 
-        Object[] data = dod.readLine(1, columns);
-        String[] line = lines.get(1);
-        for (int i = 0; i < columns.size(); i++){
-            System.out.println(data[i]);
-            System.out.println(line[i]);
-            assertEquals(data[i], line[i]);
+        data = dod.readLine(4, cols, colsSelected);
+        for (int i = 0; i < colsSelected.size(); i++) {
+            assertEquals(datas.get(4)[i], data[i]);
+            System.out.print(data[i] + ", ");
         }
+        System.out.print("\n");
 
-        data = dod.readLine(5, columns);
-        line = lines.get(5);
-        for (int i = 0; i < columns.size(); i++){
-            System.out.println(data[i]);
-            System.out.println(line[i]);
-            assertEquals(data[i], line[i]);
+        data = dod.readLine(9, cols, colsSelected);
+        for (int i = 0; i < colsSelected.size(); i++) {
+            assertEquals(datas.get(9)[i], data[i]);
+            System.out.print(data[i] + ", ");
         }
     }
 
 }
-*/
--- src/test/java/parser/CSVParserTest.java
+++ src/test/java/parser/CSVParserTest.java
@@ -1,10 +1,13 @@
-/*
 package parser;
 
+import logic.Column;
+import logic.DatasOnDisk;
+import logic.Table;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
@@ -16,56 +19,81 @@ import static org.junit.Assert.assertEquals;
 public class CSVParserTest {
 
 	private static CSVParser csvp = new CSVParser();
+    private DatasOnDisk dod = new DatasOnDisk();
 
-	private static String fileName;
+	private static File file;
 	private static URL url_CSV;
 
-	@BeforeClass
+    public CSVParserTest() throws FileNotFoundException {
+    }
+
+    @BeforeClass
 	public static void beforeClass() throws MalformedURLException {
-		fileName = "src/dogs.csv";
+		file = new File("src/resources/dogs.csv");
 		url_CSV = new URL("https://data.cityofnewyork.us/api/views/7yq2-hq9c/rows.csv");
 		//http://winterolympicsmedals.com/medals.csv
 	}
 
 	@Test
 	public void get_content_for_local_CSV() throws IOException {
-		List<String> contentGet = csvp.readFileLocal(fileName);
+        ArrayList<String> colsNames = new ArrayList<>();
+        colsNames.add("id");
+        colsNames.add("name");
+        colsNames.add("color");
+        colsNames.add("age");
 
-		List<String> content = new ArrayList<>();
-		content.add("Id,Prénom,Couleur,Age");
-		content.add("1,Titi,Jaune,5");
-		content.add("2,Médor,Noir,10");
-		content.add("3,Pitié,Noir,5");
-		content.add("4,Juju,Gris,5");
-		content.add("5,Vanille,Blanc,7");
-		content.add("6,Chocolat,Marron,12");
-		content.add("7,Milou,Blanc,3");
-		content.add("8,Idefix,Blanc,14");
-		content.add("9,Pluto,Jaune,17");
-		content.add("10,Dingo,Roux,1");
+        ArrayList<String> colsType = new ArrayList<>();
+        colsType.add("int");
+        colsType.add("String");
+        colsType.add("String");
+        colsType.add("int");
 
-		int numberLines = 11;
+        Table table = new Table("dogs", colsNames, colsType);
 
-		assertEquals(contentGet.size(), numberLines);
-		assertEquals(contentGet, content);
+        csvp.readFileLocal(file, table);
+
+        Column id = new Column("Id", "int");
+        Column prenom = new Column("Prenom", "String");
+        Column couleur = new Column("couleur", "String");
+        Column age = new Column("age", "int");
+
+        ArrayList<Column> cols = new ArrayList<>();
+        cols.add(id);
+        cols.add(prenom);
+        cols.add(couleur);
+        cols.add(age);
+
+        String[] s1 = {"1","Titi","Jaune","5"};
+        String[] s2 = {"2","Médor","Noir","10"};
+        String[] s3 = {"3","Pitié","Noir","5"};
+        String[] s4 = {"4","Juju","Gris","5"};
+        String[] s5 = {"5","Vanille","Blanc","7"};
+        String[] s6 = {"6","Chocolat","Marron","12"};
+        String[] s7 = {"7","Milou","Blanc","3"};
+        String[] s8 = {"8","Idefix","Blanc","14"};
+        String[] s9 = {"9","Pluto","Jaune","17"};
+        String[] s10 = {"10","Dingo","Roux","1"};
+
+        ArrayList<String[]> datas = new ArrayList<>();
+        datas.add(s1);
+        datas.add(s2);
+        datas.add(s3);
+        datas.add(s4);
+        datas.add(s5);
+        datas.add(s6);
+        datas.add(s7);
+        datas.add(s8);
+        datas.add(s9);
+        datas.add(s10);
+
+        Object[] data;
+        for (int i = 0; i < 10; i++) {
+            data = dod.readLine(i, cols, cols);
+            for (int j = 0; j < 4; j++) {
+                assertEquals(datas.get(i)[j], data[j]);
+                System.out.print(data[j] + ", ");
+            }
+            System.out.print("\n");
+        }
 	}
-
-	@Test
-	public void get_content_for_online_CSV() throws IOException {
-		List contentGet = csvp.readFileURL(url_CSV);
-
-		List<String> content = new ArrayList<>();
-		content.add("ProviderType,DFTA ID,ProgramName,SponsorName,ProgramAddress,ProgramCity,ProgramState,Postcode,Borough,ProgramPhone,DFTA Funded,MonHourOpen,MonHourClose,TueHourOpen,TueHourClose,WedHourOpen,WedHourClose,ThuHourOpen,ThuHourClose,FriHourOpen,FriHourClose,SatHourOpen,SatHourClose,SunHourOpen,SunHourClose,Latitude,Longitude,Community Board,Council District,Census Tract,BIN,BBL,NTA,Location1");
-		content.add("GERIATRIC MENTAL HEALTH SERVICES CONTRACTS,5G301,WEILL CORNELL MENTAL HEALTH,WEILL MEDICAL COLLEGE OF CORNELL UNIVERSITY,478 1st Street,NEW YORK,NY,10010,MANHATTAN,914-997-4333,Y,9 :00,5 :00,9 :00,5 :00,9 :00,5 :00,9 :00,5 :00,9 :00,5 :00,00:00,00:00,00:00,00:00,40.671232,-73.976579,306,39,155,3025180,3010760019,Park Slope-Gowanus,\"(40.671232, -73.976579)\"");
-		content.add("GERIATRIC MENTAL HEALTH SERVICES CONTRACTS,3G101,SPOP MENTAL HEALTH,SERVICE PROGRAM FOR OLDER PEOPLE INC,302 WEST 91ST STREET,NEW YORK,NY,10024,MANHATTAN,212-787-7120,Y,09:00,05:00,09:00,05:00,09:00,05:00,09:00,05:00,09:00,05:00,00:00,00:00,00:00,00:00,40.792171,-73.975796,107,6,179,1034085,1012510022,Upper West Side,\"(40.792171, -73.975796)\"");
-		content.add("GERIATRIC MENTAL HEALTH SERVICES CONTRACTS,4G801,SAMUEL FIELD MENTAL HEALTH,SAMUEL FIELD YM & YWHA INC,58-20 Little Neck Parkway,FLUSHING,NY,11362,QUEENS,718-225-6750,Y,8 :00,4 :00,8 :00,4 :00,8 :00,4 :00,8 :00,4 :00,8 :00,4 :00,00:00,00:00,00:00,00:00,40.761577,-73.723122,411,23,152902,4173029,4083520051,Douglas Manor-Douglaston-Little Neck,\"(40.761577, -73.723122)\"");
-		content.add("GERIATRIC MENTAL HEALTH SERVICES CONTRACTS,1G701,JASA MENTAL HEALTH,THE JEWISH ASSOCIATION FOR SERVICES FOR THE AGED,1 Fordham Plaza,BRONX,NY,10458,BRONX,718-365-4044,Y,09:00,5 :00,09:00,5 :00,09:00,5 :00,09:00,5 :00,09:00,5 :00,00:00,00:00,00:00,00:00,40.860739,-73.889585,206,15,387,2088325,2030330053,Belmont,\"(40.860739, -73.889585)\"");
-
-		int numberLines = 5;
-
-		assertEquals(contentGet.size(), numberLines);
-		assertEquals(contentGet, content);
-	}
-
 }
-*/
