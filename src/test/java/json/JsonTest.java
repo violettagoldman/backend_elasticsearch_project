88e6f52d57e6ce4ac4d287e0c83db8831c009f4f
unknown <yoyoalternatif@gmail.com>
2021-06-02 08:11:35 +0200

    test

--- src/test/java/json/JsonTest.java
+++ src/test/java/json/JsonTest.java
@@ -1,6 +1,7 @@
 package json;
 
 import dataBase.DataBaseTest;
+import io.vertx.core.json.JsonObject;
 import logic.DataBase;
 import logic.Table;
 import logic.json.jsonTable.JsonTable;
@@ -8,16 +9,14 @@ import org.junit.Test;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import static logic.json.jsonGet.JsonGet.json;
 
 import static logic.json.jsonIndex.JsonIndex.jsonIndex;
+import static logic.json.jsonTable.JsonTable.jsonTable;
 import static org.junit.Assert.assertEquals;
 
 public class JsonTest {
-
 
     @Test
     public void jsonGet() throws IOException, NoSuchAlgorithmException {
@@ -28,6 +27,15 @@ public class JsonTest {
         final String data = "{\n" +
                 "    \"method\": \"select\",\n" +
                 "    \"table\": \"dogs\",\n" +
+                "  \t\"columns\":\n" +
+                "    [\n" +
+                "      {\n" +
+                "        \"column\": \"Couleur\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"column\": \"Age\"\n" +
+                "      }\n" +
+                "    ],\n" +
                 "  \t\"args\":\n" +
                 "    [\n" +
                 "      {\n" +
@@ -44,22 +52,18 @@ public class JsonTest {
                 "    ]\n" +
                 "}";
 
-       // String str = json(data);
-      //  System.out.println(str);
+        JsonObject dataObject = new JsonObject(data);
+        JsonObject result1 = json(dataObject);
+        System.out.println(result1);
 
-        String str2 =
-                "Nom de la table : result\n" +
-                "id | Ville Départ | Ville arrivée |\n" +
-                " 0 | Paris | Bordeaux |\n" +
-                " 1 | Paris | Lavandia |\n" +
-                " 3 | Paris | Test |\n";
+        String result2 = "{\"lines\":[{\"Couleur\":\"Noir\",\"Age\":\"10\"},{\"Couleur\":\"Noir\",\"Age\":\"5\"}]}";
 
-        //assertEquals(str,str2);
+        assertEquals(result1.toString(),result2);
     }
 
     @Test
     public void jsonIndex() throws IOException, NoSuchAlgorithmException {
-        /*
+
         final String data = "{\n" +
                 "    \"table_name\": \"Table_Name\",\n" +
                 "  \t\"columns\":\n" +
@@ -75,12 +79,11 @@ public class JsonTest {
 
         String[] str = logic.json.jsonIndex.JsonIndex.jsonIndex(data);
         System.out.println(str);
-        */
     }
 
     @Test
     public void jsonTable() throws IOException, NoSuchAlgorithmException {
-        /*
+
         final String data = "{\n" +
                 "    \"table_name\": \"Table_Name\",\n" +
                 "  \t\"columns\":\n" +
@@ -96,15 +99,23 @@ public class JsonTest {
                 "    ]\n" +
                 "}";
 
-        ArrayList str = (ArrayList) JsonTable.jsonTable(data, true);
-        System.out.println(str);
+        ArrayList resultNames = (ArrayList) JsonTable.jsonTable(data, true);
+        ArrayList resultTypes = (ArrayList) JsonTable.jsonTable(data, false);
 
-        Map<String, String> str2 = new HashMap<String, String>();
-        str2.put("Couleur", "String");
-        str2.put("Age", "Int");
+        System.out.println(resultNames);
+        System.out.println(resultTypes);
 
-        //assertEquals(str,str2);
+        ArrayList resultNames2 = new ArrayList();
+        resultNames2.add("Couleur");
+        resultNames2.add("Age");
+        System.out.println(resultNames2);
 
-         */
+        ArrayList resultTypes2 = new ArrayList();
+        resultTypes2.add("String");
+        resultTypes2.add("Int");
+        System.out.println(resultTypes2);
+
+        assertEquals(resultNames,resultNames2);
+        assertEquals(resultTypes,resultTypes2);
     }
 }
